20a083ab29ff35145fcb5b28348cbe24534319ec
tulakann <tul@salweensolutions.com>
2018-04-04 14:20:16 +0700

    add cocoapod, miniconda, change miniconda path

--- src/os/install/macos/misc_tools.sh
+++ src/os/install/macos/misc_tools.sh
@@ -10,6 +10,12 @@ print_in_purple "\n   Miscellaneous Tools\n\n"
 
 brew_install "ShellCheck" "shellcheck"
 
+# terminal
+brew_install "iTerm2" "iterm2"
+
+# python
+brew_install "Miniconda" "miniconda" "caskroom/cask" "cask"
+
 # ms stuffs
 brew_install "Visual Studio Code" "visual-studio-code" "caskroom/cask" "cask"
 brew_install ".NET SDK" "dotnet-sdk" "caskroom/cask" "cask"
@@ -21,6 +27,9 @@ brew_install "Java 8" "java8" "caskroom/versions" "cask"
 brew_install "Android SDK" "android-sdk" "caskroom/cask" "cask"
 brew_install "Android NDK" "android-ndk" "caskroom/cask" "cask"
 brew_install "Android Platform Tools" "android-platform-tools" "caskroom/cask" "cask"
+
+# for running cordova ios
+brew_install "Cocoapods" "cocoapods"
 
 # need to
 # sdkmanager "platforms;android-27"
